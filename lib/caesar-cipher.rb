d2e544ed5e9ae66e6a2b54f4f4a5c5f609aae972
Deetss <dylan817@live.com>
2017-09-16 11:35:54 -0500

    Catches nil input

--- lib/caesar-cipher.rb
+++ lib/caesar-cipher.rb
@@ -2,18 +2,22 @@
 def caesar_cipher text, count
     new_text = ""
     count = count.to_i
-    text.each_char do |letter|
-        if letter.match(/[a-zA-Z]/)
-            count.times do |i|
-                if letter == "z"
-                    letter = "a"
-                elsif letter == "Z"
-                    letter = "A"
-                else
-                    letter = letter.next
+    if text.nil?
+        return
+    else
+        text.each_char do |letter|
+            if letter.match(/[a-zA-Z]/)
+                count.times do |i|
+                    if letter == "z"
+                        letter = "a"
+                    elsif letter == "Z"
+                        letter = "A"
+                    else
+                        letter = letter.next
+                    end
                 end
+                new_text << letter
             end
-            new_text << letter
         end
     end
     new_text
